6d7f5938cbaa3aadaee460852580069f33d674e4
BrainessCompany <brainesscompany@gmail.com>
2024-09-06 16:32:34 -0300

    mudei nome do modulo

--- __manifest__.py
+++ __manifest__.py
@@ -2,11 +2,11 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Declaração de Importação",
+    "name": "Brainess Importação",
     "version": "14.0.1.0.0",
     "license": "AGPL-3",
-    "author": "KMEE",
-    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "author": "brainess",
+    "website": "",
     "depends": [
         "mail",
         "l10n_br_account",
