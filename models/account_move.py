b541b9ca7d3735b15c7c6cca4946671f4c89788f
BrainessCompany <brainesscompany@gmail.com>
2024-09-23 18:45:54 -0300

    agora ajustei o codigo acount movea

--- models/account_move.py
+++ models/account_move.py
@@ -6,22 +6,24 @@ class AccountMove(models.Model):
     def _compute_custom_subtotal(self):
         """
         Função personalizada para calcular o subtotal da fatura, somando valores
-        como ii_value, ipi_value, cofins_value, freight_value e price_unit * quantity.
-        TODO: FAZER ISSO SÓ PARA FATURAS DE IMPORTAÇÃO
+        como ii_value, ipi_value, cofins_value, freight_value, e price_unit * quantity.
+        Este cálculo será aplicado apenas para faturas de importação.
         """
         for move in self:
-            for line in move.line_ids:
-                fiscal_line = line.fiscal_document_line_id
-                if fiscal_line:
-                    # Somar os campos desejados da tabela l10n_br_fiscal_document_line
-                    subtotal = (
-                        fiscal_line.amount_tax_included +
-                        fiscal_line.freight_value +
-                        fiscal_line.other_value +
-                        (line.price_unit * line.quantity)
-                    )
-                    # Atualizar o campo price_subtotal da linha de fatura
-                    line.price_subtotal = subtotal
+            if move.fiscal_operation_id and move.fiscal_operation_id.import_operation:
+                # Apenas para faturas de importação
+                for line in move.line_ids:
+                    fiscal_line = line.fiscal_document_line_id
+                    if fiscal_line:
+                        # Somar os campos desejados da tabela l10n_br_fiscal_document_line
+                        subtotal = (
+                            fiscal_line.amount_tax_included +  # Valor de impostos
+                            fiscal_line.freight_value +  # Valor do frete
+                            fiscal_line.other_value +  # Outros valores adicionais
+                            (line.price_unit * line.quantity)  # Preço unitário * quantidade
+                        )
+                        # Atualizar o campo price_subtotal da linha de fatura
+                        line.price_subtotal = subtotal
 
     @api.model
     def create(self, vals):
