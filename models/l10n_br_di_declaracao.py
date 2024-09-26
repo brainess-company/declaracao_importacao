d31f23489982df30d722a47ecafb941e92321fea
BrainessCompany <brainesscompany@gmail.com>
2024-09-26 19:16:10 -0300

    .

--- models/l10n_br_di_declaracao.py
+++ models/l10n_br_di_declaracao.py
@@ -55,6 +55,192 @@ class L10nBrDiDeclaracao(models.Model):
     carga_peso_bruto = fields.Float(digits=(12, 7))
     carga_peso_liquido = fields.Float(digits=(12, 7))
 
+    # Campos Extras
+
+    arquivo_declaracao = fields.Binary(
+        attachment=True,
+    )
+
+    @api.model
+    def _default_fiscal_operation(self):
+        return self.env.company.import_trade_fiscal_operation_id
+
+    @api.model
+    def _fiscal_operation_domain(self):
+        domain = [("state", "=", "approved")]
+        return domain
+
+    state = fields.Selection(
+        selection=[
+            ("draft", "Draft"),
+            ("open", "Open"),
+            ("locked", "Loked"),
+            ("canceled", "Canceled"),
+        ],
+        default="draft",
+        required=True,
+        copy=False,
+        tracking=True,
+    )
+
+    fiscal_operation_id = fields.Many2one(
+        comodel_name="l10n_br_fiscal.operation",
+        readonly=True,
+        states={"draft": [("readonly", False)]},
+        default=_default_fiscal_operation,
+        domain=lambda self: self._fiscal_operation_domain(),
+    )
+
+    account_move_id = fields.Many2one(
+        comodel_name="account.move",
+        readonly=True,
+    )
+
+    company_id = fields.Many2one(
+        "res.company",
+        default=lambda self: self.env.company,
+    )
+
+    currency_id = fields.Many2one(
+        "res.currency",
+        default=lambda self: self.env.ref("base.BRL").id,
+        readonly=True,
+    )
+
+    dolar_currency_id = fields.Many2one(
+        "res.currency",
+        default=lambda self: self.env.ref("base.USD").id,
+        readonly=True,
+    )
+
+    freight_currency_id = fields.Many2one(
+        "res.currency",
+    )
+
+    insurance_currency_id = fields.Many2one(
+        "res.currency",
+    )
+
+    # Relacionais XML
+
+    di_adicao_ids = fields.One2many("declaracao_importacao.adicao", "declaracao_id")
+    di_despacho_ids = fields.One2many("declaracao_importacao.despacho", "declaracao_id")
+    di_mercadoria_ids = fields.One2many("declaracao_importacao.mercadoria", "declaracao_id")
+    di_pagamento_ids = fields.One2many("declaracao_importacao.pagamento", "declaracao_id")
+    di_valor_ids = fields.One2many("declaracao_importacao.valor", "declaracao_id")
+
+    # Campos do arquivo XML
+
+    numero_di = fields.Char()
+    data_registro = fields.Date()
+    sequencial_retificacao = fields.Char()
+
+    armazenamento_recinto_aduaneiro_codigo = fields.Char()
+    armazenamento_recinto_aduaneiro_nome = fields.Char()
+    armazenamento_setor = fields.Char()
+
+    canal_selecao_parametrizada = fields.Char()
+    caracterizacao_operacao_codigo_tipo = fields.Char()
+    caracterizacao_operacao_descricao_tipo = fields.Char()
+
+    carga_data_chegada = fields.Date()
+    carga_numero_agente = fields.Char()
+    carga_pais_procedencia_codigo = fields.Char()
+    carga_pais_procedencia_nome = fields.Char()
+    carga_peso_bruto = fields.Float(digits=(12, 7))
+    carga_peso_liquido = fields.Float(digits=(12, 7))
+    carga_urf_entrada_codigo = fields.Char()
+    carga_urf_entrada_nome = fields.Char()
+
+    conhecimento_carga_embarque_data = fields.Date()
+    conhecimento_carga_embarque_local = fields.Char()
+    conhecimento_carga_id = fields.Char()
+    conhecimento_carga_tipo_codigo = fields.Char()
+    conhecimento_carga_tipo_nome = fields.Char()
+    conhecimento_carga_utilizacao = fields.Char()
+    conhecimento_carga_utilizacao_nome = fields.Char()
+
+    documento_chegada_carga_codigo_tipo = fields.Char()
+    documento_chegada_carga_nome = fields.Char()
+    documento_chegada_carga_numero = fields.Char()
+
+    data_desembaraco = fields.Date()
+
+    frete_collect = fields.Float()
+    frete_em_territorio_nacional = fields.Monetary(currency_field="dolar_currency_id")
+    frete_moeda_negociada_codigo = fields.Char()
+    frete_moeda_negociada_nome = fields.Char()
+    frete_prepaid = fields.Monetary(currency_field="dolar_currency_id")
+    frete_total_dolares = fields.Monetary(currency_field="dolar_currency_id")
+    frete_total_moeda = fields.Monetary(currency_field="freight_currency_id")
+    frete_total_reais = fields.Monetary()
+
+    icms = fields.Char()
+
+    agencia_icms = fields.Char()
+    banco_icms = fields.Char()
+    codigo_tipo_recolhimento_icms = fields.Char()
+    cpf_responsavel_registro = fields.Char()
+    data_pagamento_icms = fields.Char()
+    data_registro_icms = fields.Char()
+    hora_registro_icms = fields.Char()
+    nome_tipo_recolhimento_icms = fields.Char()
+    numero_sequencial_icms = fields.Char()
+    uf_icms = fields.Char()
+    valor_total_icms = fields.Char()
+
+
+    importador_codigo_tipo = fields.Char()
+    importador_cpf_representante_legal = fields.Char()
+    importador_endereco_bairro = fields.Char()
+    importador_endereco_cep = fields.Char()
+    importador_endereco_complemento = fields.Char()
+    importador_endereco_logradouro = fields.Char()
+    importador_endereco_municipio = fields.Char()
+    importador_endereco_numero = fields.Char()
+    importador_endereco_uf = fields.Char()
+    importador_nome = fields.Char()
+    importador_nome_representante_legal = fields.Char()
+    importador_numero = fields.Char()
+    importador_numero_telefone = fields.Char()
+
+    local_descarga_total_dolares = fields.Monetary(currency_field="dolar_currency_id")
+    local_descarga_total_reais = fields.Monetary()
+    local_embarque_total_dolares = fields.Monetary(currency_field="dolar_currency_id")
+    local_embarque_total_reais = fields.Monetary()
+
+    modalidade_despacho_codigo = fields.Char()
+    modalidade_despacho_nome = fields.Char()
+
+    operacao_fundap = fields.Char()
+
+    seguro_moeda_negociada_codigo = fields.Char()
+    seguro_moeda_negociada_nome = fields.Char()
+    seguro_total_dolares = fields.Monetary(currency_field="dolar_currency_id")
+    seguro_total_moeda_negociada = fields.Monetary(
+        currency_field="insurance_currency_id"
+    )
+    seguro_total_reais = fields.Monetary()
+
+    situacao_entrega_carga = fields.Char()
+    tipo_declaracao_codigo = fields.Char()
+    tipo_declaracao_nome = fields.Char()
+    total_adicoes = fields.Char()
+
+    urf_despacho_codigo = fields.Char()
+    urf_despacho_nome = fields.Char()
+
+    valor_total_multa_arecolher_ajustado = fields.Float()
+
+    via_transporte_codigo = fields.Char()
+    via_transporte_multimodal = fields.Char()
+    via_transporte_nome = fields.Char()
+    via_transporte_nome_transportador = fields.Char()
+    via_transporte_numero_veiculo = fields.Char()
+    via_transporte_pais_transportador_codigo = fields.Char()
+
+    informacao_complementar = fields.Text()
+
     # Método para processar a declaração de importação a partir do XML
     def importa_declaracao(self, arquivo=False):
         if not self.arquivo_declaracao:
